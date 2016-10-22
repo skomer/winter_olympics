67f921399cf9d3a9f5b8d741359473f32a4ff951
Josephine Elder <parusmontanus@gmail.com>
2016-10-22 12:47:29 +0100

    Wrote db SQL

---
+++ db/winter_olympics.sql
@@ -0,0 +1,38 @@
+DROP TABLE IF EXISTS athletes_events;
+DROP TABLE IF EXISTS events;
+DROP TABLE IF EXISTS athletes;
+DROP TABLE IF EXISTS nations;
+DROP TABLE IF EXISTS medals;
+
+CREATE TABLE nations (
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255)
+  );
+
+CREATE TABLE medals (
+  id SERIAL4 PRIMARY KEY,
+  type VARCHAR(255)
+);
+
+CREATE TABLE athletes (
+  id SERIAL4 PRIMARY KEY,
+  first_name VARCHAR(255),
+  last_name VARCHAR(255),
+  nation_id INT4 REFERENCES nations(id) ON DELETE CASCADE
+);
+
+CREATE TABLE events (
+  id SERIAL4 PRIMARY KEY,
+  title VARCHAR(255),
+  venue VARCHAR(255),
+  status VARCHAR(255)
+);
+
+CREATE TABLE athletes_events (
+  id SERIAL4 PRIMARY KEY,
+  status VARCHAR(255),
+  position INT2,
+  athlete_id INT4 REFERENCES athletes(id) ON DELETE CASCADE,
+  event_id INT4 REFERENCES events(id) ON DELETE CASCADE,
+  medal_id INT4 REFERENCES medals(id) ON DELETE CASCADE
+);
