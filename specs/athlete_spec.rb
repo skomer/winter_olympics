e4810a83833b3dd40ae525290122ce20a82bb17a
Josephine Elder <parusmontanus@gmail.com>
2016-10-22 15:09:17 +0100

    Wrote athlete test

--- specs/athlete_spec.rb
+++ specs/athlete_spec.rb
@@ -4,5 +4,20 @@ require_relative '../models/athlete.rb'
 
 class TestAthlete < MiniTest::Test
 
+  def setup
+    options = {
+      'first_name' => "Jenny",
+      'last_name' => "Jones",
+      'name_convention' => "first last",
+      'nation_id' => nil
+    }
+    @athlete = Athlete.new(options)
+  end
+
+  def test_athlete_has_last_name
+    assert_equal("Jones", @athlete.last_name)
+  end
+
+
 
 end
