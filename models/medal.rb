88ae66f3eed21f8f23c6c42eb0348ec2f02bfbc7
Josephine Elder <parusmontanus@gmail.com>
2016-10-22 15:00:40 +0100

    Wrote Nation test

--- models/medal.rb
+++ models/medal.rb
@@ -1,4 +1,4 @@
-
+require_relative '../db/sql_runner.rb'
 
 class Medal
 
--- specs/nation_spec.rb
+++ specs/nation_spec.rb
@@ -4,5 +4,15 @@ require_relative '../models/nation.rb'
 
 class TestNation < MiniTest::Test
 
+  def setup
+    options = {
+      'name' => 'Switzerland'
+    }
+    @nation = Nation.new(options)
+  end
+
+  def test_nation_has_name
+    assert_equal("Switzerland", @nation.name)
+  end
 
 end
