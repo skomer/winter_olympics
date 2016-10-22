4049640f91da746c66ae56f3ba2b7d0cc9c36763
Josephine Elder <parusmontanus@gmail.com>
2016-10-22 15:22:13 +0100

    Wrote athlete_event test

--- specs/athlete_event_spec.rb
+++ specs/athlete_event_spec.rb
@@ -4,5 +4,20 @@ require_relative '../models/athlete_event.rb'
 
 class TestAthleteEvent < MiniTest::Test
 
+  def setup
+    options = {
+      'status' => "",
+      'position' => "",
+      'athlete_id' => 76,
+      'event_id' => 12,
+      'medal_id' => 0
+    }
+    @athlete_event = AthleteEvent.new(options)
+  end
+
+  def test_athlete_event_has_event_id
+    assert_equal(12, @athlete_event.event_id)
+  end
+
 
 end
