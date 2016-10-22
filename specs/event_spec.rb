7c903e06de7c04f319dda78d4190718de0e33756
Josephine Elder <parusmontanus@gmail.com>
2016-10-22 15:15:55 +0100

    Wrote event test

--- specs/event_spec.rb
+++ specs/event_spec.rb
@@ -4,5 +4,20 @@ require_relative '../models/event.rb'
 
 class TestEvent < MiniTest::Test
 
+  def setup
+    options = {
+      'title' => "Alpine Super G",
+      'venue' => "Jeongseon",
+      'status' => "not occurred"
+    }
+    @event = Event.new(options)
+  end
+
+  def test_event_status
+    assert_equal("not occurred", @event.status)
+  end
+
+  
+
 
 end
