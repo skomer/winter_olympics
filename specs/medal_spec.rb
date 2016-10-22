89d05a1e65f7335124b60700337d51d1faeec33f
Josephine Elder <parusmontanus@gmail.com>
2016-10-22 14:53:24 +0100

    Wrote medal test

--- specs/medal_spec.rb
+++ specs/medal_spec.rb
@@ -4,5 +4,15 @@ require_relative '../models/medal.rb'
 
 class TestMedal < MiniTest::Test
 
+  def setup
+    options = {
+      'type' => 'gold'
+    }
+    @medal = Medal.new(options)
+  end
+
+  def test_medal_is_gold
+    assert_equal("gold", @medal.type)
+  end
 
 end
